f54baae033798f74ef0bf96b76f3f0377d38ee0b
Alex Burneikis <z5481067@ad.unsw.edu.au>
2026-01-21 06:48:44 +1100

    noice cmdline

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -2,21 +2,46 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = { "nvim-tree/nvim-web-devicons" },
   event = "VeryLazy",
-  opts = {
-    options = {
-      theme = "tokyonight",
-      globalstatus = true,
-      component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
-    },
-    sections = {
-      lualine_a = { "mode" },
-      lualine_b = { "branch", "diff", "diagnostics" },
-      lualine_c = { { "filename", path = 1 } },
-      lualine_x = { "encoding", "fileformat", "filetype" },
-      lualine_y = { "progress" },
-      lualine_z = { "location" },
-    },
-    extensions = { "lazy", "trouble" },
-  },
+  opts = function()
+    local noice_search = {
+      function()
+        return require("noice").api.status.search.get()
+      end,
+      cond = function()
+        return package.loaded["noice"] and require("noice").api.status.search.has()
+      end,
+    }
+    local noice_mode = {
+      function()
+        return require("noice").api.status.mode.get()
+      end,
+      cond = function()
+        return package.loaded["noice"] and require("noice").api.status.mode.has()
+      end,
+    }
+
+    return {
+      options = {
+        theme = "tokyonight",
+        globalstatus = true,
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" },
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { { "filename", path = 1 } },
+        lualine_x = {
+          noice_search,
+          noice_mode,
+          "encoding",
+          "fileformat",
+          "filetype",
+        },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      extensions = { "lazy", "trouble" },
+    }
+  end,
 }
