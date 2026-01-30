13b094a891e9c1af8a7bd59f8cdaecc06effe832
Alex Burneikis <z5481067@ad.unsw.edu.au>
2026-01-30 22:56:20 +1100

    yank path

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Yank file paths
+vim.keymap.set("n", "<leader>yp", function()
+  local path = vim.fn.expand("%:.")
+  vim.fn.setreg("+", path)
+  vim.notify('Yanked relative path: ' .. path, vim.log.levels.INFO)
+end, { desc = "Yank relative path" })
+
+vim.keymap.set("n", "<leader>yP", function()
+  local path = vim.fn.expand("%:p")
+  vim.fn.setreg("+", path)
+  vim.notify('Yanked full path: ' .. path, vim.log.levels.INFO)
+end, { desc = "Yank full path" })
