af880a727fa964875108ee3ca0dd0db3be732156
Alex Burneikis <z5481067@ad.unsw.edu.au>
2026-02-01 08:29:02 +1100

    use illuminate with angular fix

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -1,9 +1,33 @@
 return {
 	"RRethy/vim-illuminate",
 	config = function()
-		require("illuminate").configure({
+		local illuminate = require("illuminate")
+
+		illuminate.configure({
 			providers = { "lsp", "treesitter", "regex" },
 			delay = 100,
+		})
+
+		vim.api.nvim_create_autocmd("BufEnter", {
+			pattern = "*.component.ts",
+			callback = function()
+				illuminate.configure({
+					providers = { "treesitter", "regex" },
+					delay = 100,
+				})
+				vim.opt_local.iskeyword:append("-")
+			end,
+		})
+
+		-- Restore LSP for other TS files
+		vim.api.nvim_create_autocmd("BufEnter", {
+			pattern = "*.ts",
+			callback = function()
+				if not vim.fn.expand("%"):match("%.component%.ts$") then illuminate.configure({
+					providers = { "lsp", "treesitter", "regex" },
+					delay = 100,
+				}) end
+			end,
 		})
 	end,
 }
