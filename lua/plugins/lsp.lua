e0848eee580b81d52f1be81704aa17a49c22b29d
Alex Burneikis <z5481067@ad.unsw.edu.au>
2026-02-07 11:55:29 +1100

    fix deprecated error

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -140,14 +140,16 @@ return {
 			-- Enable configured servers
 			vim.lsp.enable({ "lua_ls", "vtsls", "angularls", "pyright", "eslint" })
 
-			-- Diagnostic signs
-			local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
-			for type, icon in pairs(signs) do
-				local hl = "DiagnosticSign" .. type
-				vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-			end
-
+			-- Diagnostic configuration
 			vim.diagnostic.config({
+				signs = {
+					text = {
+						[vim.diagnostic.severity.ERROR] = " ",
+						[vim.diagnostic.severity.WARN] = " ",
+						[vim.diagnostic.severity.HINT] = " ",
+						[vim.diagnostic.severity.INFO] = " ",
+					},
+				},
 				virtual_text = {
 					prefix = "●",
 				},
