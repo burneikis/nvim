dafa6d35f1ffbdf70acf0e54dd6f644a47214a41
Your Name <alexburneikis@gmail.com>
2026-02-12 19:51:11 +1100

    cleanup

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -14,6 +14,36 @@ return {
 		local finders = require("telescope.finders")
 		local conf = require("telescope.config").values
 		local previewers = require("telescope.previewers")
+		local builtin = require("telescope.builtin")
+
+		-- Git status picker with merge-base support
+		local function git_status_picker()
+			if vim.g.gitsigns_merge_base then
+				-- Show files changed relative to merge-base with diff preview
+				local merge_base = vim.fn.systemlist("git merge-base HEAD main")[1]
+				if merge_base and merge_base ~= "" then
+					pickers
+						.new({}, {
+							prompt_title = "Changed Files (vs merge-base " .. merge_base:sub(1, 7) .. ")",
+							finder = finders.new_oneshot_job({ "git", "diff", "--name-only", merge_base }),
+							sorter = conf.file_sorter({}),
+							previewer = previewers.new_termopen_previewer({
+								title = "Git File Diff Preview",
+								get_command = function(entry)
+									return { "git", "--no-pager", "diff", merge_base, "--", entry.value }
+								end,
+							}),
+						})
+						:find()
+				else
+					vim.notify("Could not determine merge-base", vim.log.levels.WARN)
+					builtin.git_status()
+				end
+			else
+				-- Default behavior: show working tree status
+				builtin.git_status()
+			end
+		end
 
 		telescope.setup({
 			defaults = {
@@ -54,7 +84,7 @@ return {
 
 		telescope.load_extension("fzf")
 
-		local builtin = require("telescope.builtin")
+		-- Keymaps
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Files" })
 		vim.keymap.set("n", "<leader>ft", builtin.live_grep, { desc = "Text (grep)" })
 		vim.keymap.set("n", "<leader>fr", builtin.oldfiles, { desc = "Recent" })
@@ -63,30 +93,6 @@ return {
 		vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Commands" })
 		vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Diagnostics" })
 		vim.keymap.set("n", "<leader>fs", builtin.lsp_document_symbols, { desc = "Document symbols" })
-		vim.keymap.set("n", "<leader>fg", function()
-			if vim.g.gitsigns_merge_base then
-				-- Show files changed relative to merge-base with diff preview
-				local merge_base = vim.fn.systemlist("git merge-base HEAD main")[1]
-				if merge_base and merge_base ~= "" then
-					pickers
-						.new({}, {
-							prompt_title = "Changed Files (vs merge-base " .. merge_base:sub(1, 7) .. ")",
-							finder = finders.new_oneshot_job({ "git", "diff", "--name-only", merge_base }),
-							sorter = conf.file_sorter({}),
-							previewer = previewers.new_termopen_previewer({
-								title = "Git File Diff Preview",
-								get_command = function(entry) return { "git", "--no-pager", "diff", merge_base, "--", entry.value } end,
-							}),
-						})
-						:find()
-				else
-					vim.notify("Could not determine merge-base", vim.log.levels.WARN)
-					builtin.git_status()
-				end
-			else
-				-- Default behavior: show working tree status
-				builtin.git_status()
-			end
-		end, { desc = "Git status" })
+		vim.keymap.set("n", "<leader>fg", git_status_picker, { desc = "Git status" })
 	end,
 }
